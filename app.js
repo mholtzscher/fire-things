3f327d684de41bc7d8e6b0e660362557b7d4122f
Michael Holtzscher <thenerdproject1@gmail.com>
2016-07-03 19:57:58 -0500

    fixing port registration

--- app.js
+++ app.js
@@ -24,6 +24,6 @@ app.post('/events', jsonParser, function(req, res) {
     res.send('OK');
 });
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
     console.log('Example app listening on port 3000!');
 });
