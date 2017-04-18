e3353809117f58b42b5544cc9b9c2b6a3389b53d
Michael Holtzscher <michael.holtzscher@cerner.com>
2017-04-18 14:18:58 -0500

    clean up functions code

--- functions/index.js
+++ functions/index.js
@@ -5,13 +5,6 @@ admin.initializeApp(functions.config().firebase);
 var api_key_http_header = 'API_KEY';
 var api_key = functions.config().fire_things.api_key;
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-
-exports.helloWorld = functions.https.onRequest((request, response) => {
-    response.send("Hello from Firebase!");
-});
-
 exports.addEvent = functions.https.onRequest((request, response) => {
     if (!request.get(api_key_http_header) || request.get(api_key_http_header) !== api_key) {
         response.sendStatus(403);
